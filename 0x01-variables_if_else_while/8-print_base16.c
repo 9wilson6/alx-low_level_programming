335b1f8c73de4eaa5d7c95ebf35f2e09790a400d
Wilson Gatheru <gatheruwilson@gmail.com>
2022-10-15 03:35:07 -0700

    F is 16

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,7 +12,7 @@ int main(void)
 
 	for (num = 48; num < 58; num++)
 		putchar(num);
-	for (ch = 'a'; ch < 'f'; ch++)
+	for (ch = 'a'; ch <= 'f'; ch++)
 		putchar(ch);
 	putchar('\n');
 	return (0);
