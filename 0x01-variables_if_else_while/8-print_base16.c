0de4016dbc9793f9df165c253c416b987afa79a7
Wilson Gatheru <gatheruwilson@gmail.com>
2022-10-15 03:29:20 -0700

    print  base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <std.io>
+
+
+/**
+ *  main - print numbers of base 16
+ *  Return: 0 for successful run
+ */
+ int main(void)
+{
+	int num;
+	char ch;
+
+	for (num = 48; num < 58; num++)
+		putchar(num);
+	for (ch = 'a'; ch < f; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
