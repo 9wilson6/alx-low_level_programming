58ed06b70718ca77f305743b7edb681a8e42e86a
Wilson Gatheru <gatheruwilson@gmail.com>
2022-10-15 01:55:05 -0700

    print alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphets in lowercase
+ * Return: 0 for a success
+ */
+
+int main(void)
+{
+	ch = 'a';
+
+	while (ch < 'z')
+		printf("%c", ch);
+		ch++;
+	printf("\n");
+}
