a0560b75e5981a8b9be3cb078fdd23c9a23edb42
Wilson Gatheru <gatheruwilson@gmail.com>
2022-10-15 03:46:23 -0700

    print combinations of 0 to 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ *  main - print 0 to 9 seperated by comma
+ *  Return: 0 for succesful execution
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+		putchar(n);
+		if (n != 57)
+			putchar(',');
+			putcar(' ');
+	putchar('\n');
+	return (0);
+}
