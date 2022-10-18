bb04d33c7002a850fcae9ad91aae0db987b5981b
Wilson Gatheru <gatheruwilson@gmail.com>
2022-10-18 07:37:41 -0700

    Print alphabets

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints alphabet in lower case then newline
+ */
+
+void print_alphabet(void)
+{
+	char ch ='a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
