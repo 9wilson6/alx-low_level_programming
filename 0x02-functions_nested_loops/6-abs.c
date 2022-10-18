c9c0d154ae287f7ab831a8822b646f88e7d16595
Wilson Gatheru <gatheruwilson@gmail.com>
2022-10-18 09:02:21 -0700

    Make it abs for me

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *  _abs - checks if a number is absolute if not makes it so
+ *  @n: number to check
+ *  Return: absolute n
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+	return (n);
+	}
+	return (-n);
+}
