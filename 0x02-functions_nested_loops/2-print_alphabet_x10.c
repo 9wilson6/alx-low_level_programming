a1c1124aa4a39da2499c892d48ccb6d9cd545196
Wilson Gatheru <gatheruwilson@gmail.com>
2022-10-18 08:04:01 -0700

    test once more

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -9,16 +9,16 @@ void print_alphabet_x10(void)
 	char ch;
 	int i = 0;
 
-	while (i <= 'a')
+	while (i <= 10)
 	{
 		ch = 'a';
 
 		while (ch <= 'z')
 		{
 			_putchar(ch);
-			_putchar('\n');
 			ch++;
 		}
+		_putchar('\n');
 		i++;
 	}
 	_putchar('\n');
