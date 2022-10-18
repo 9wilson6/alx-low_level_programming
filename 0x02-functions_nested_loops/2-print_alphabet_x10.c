65889ef89589d1f37cb3d96e13d91463893f2a07
Wilson Gatheru <gatheruwilson@gmail.com>
2022-10-18 07:46:40 -0700

    alphabet 10 times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print small case alphabet 10 times
+ */
+
+void print_alphabet_x10(void)
+{
+	char ch;
+	int i;
+
+	for (i = 0; i <= 10; i++)
+	{
+		ch = 'a';
+
+		while (ch <= 'z')
+		{
+			_putchar(ch);
+			_putchar('\n');
+		}
+	}
+	_putchar('\n');
+}
