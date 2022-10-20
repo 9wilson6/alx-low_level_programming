42f454064f7d37e3f0eafed62750e5c9375ca889
Wilson Gatheru <gatheruwilson@gmail.com>
2022-10-20 07:07:25 -0700

    Pass 0 as args

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - test if a function that checks if number is
+ * positive or negative works when 0 is passed as the number.
+ * Return: 0 (success)
+ */
+
+int main(void)
+{
+	positive_or_negative(i);
+
+	return (0);
+}
