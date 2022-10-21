773061928d52119d830daaba8804c21aaf05e6f5
Wilson Gatheru <gatheruwilson@gmail.com>
2022-10-20 23:42:04 -0700

    No more infinate loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -2,13 +2,13 @@
 
 /**
  *  main - causes an infinite loop
- *  Return: 0
+ *Return: 0
  */
 
 int main(void)
 {
-	 int i;
-
+	int i;
+	
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
@@ -17,7 +17,6 @@ int main(void)
 /*	{*/
 /*		putchar(i);*/
 /*	}*/
-
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
