cf2ea9737320278e14621d3c2991ba367c067a6d
Wilson Gatheru <gatheruwilson@gmail.com>
2022-11-16 01:04:32 -0800

    create dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -64,7 +64,9 @@ dog_t *new_dog(char *name, float age, char *owner)
 
 	dog = malloc(sizeof(dog_t));
 	if (dog == NULL)
+	{
 		return (NULL);
+	}
 	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->owner == NULL)
 	{
