b146414e62314f00ec5f8a61e675290f8585826e
Wilson Gatheru <gatheruwilson@gmail.com>
2022-11-15 23:45:23 -0800

    dog h

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -9,16 +9,15 @@
  *  Description: Longer description
  */
 
-struct dog
+typedef struct
 {
 	char *name;
 	float age;
 	char *owner;
-}
+}dog;
 
 
-
-typedef struct dog dog_t;
+dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
