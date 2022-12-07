1370c4baf891949fa71ae7f77bf53dfe4f99d431
9wilson6 <gatheruwilson@gmail.com>
2022-12-07 06:28:16 -0800

    create a file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -21,7 +21,9 @@ int create_file(const char *filename, char *text_content)
 	if (text_content == NULL)
 		text_content = "";
 	while (text_content[i] != '\0')
+	{
 		i++;
+	}
 	file = open(filename, O_CREAT | O_WRONGLY | O_TRUNC, 0600);
 	if (file == -1)
 		return (-1);
