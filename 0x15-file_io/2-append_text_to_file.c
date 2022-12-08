0d04c4c0603316b4a7d37115ed2eb417d3ad3897
9wilson6 <gatheruwilson@gmail.com>
2022-12-08 06:49:27 -0800

    append text

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -12,30 +12,26 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
-	int file;
-	int i = 0;
+	int fd;
+	int nletters;
+	int rwr;
 
-	if (filename == NULL)
-	{
+	if (!filename)
 		return (-1);
-	}
+	fd = open(filename, O_WRONLY | O_APPEND);
 
-	if (text_content == NULL)
-	{
-		text_content = "";
-	}
-
-	while (text_content[i] != '\0')
-	{
-		i++;
-	}
-
-	file = open(filename O_WRONGLY | O_APPEND);
-	if (file == -1)
-	{
+	if (fd == -1)
 		return (-1);
+	if (text_content)
+	{
+		for (nletters = 0; text_content[nletters]; nletters++)
+			;
+		rwr = write(fd, text_content, nletters);
+
+		if (rwr == -1)
+			return (-1);
 	}
 
-	write(file, text_content, i);
+	close(fd);
 	return (1);
 }
